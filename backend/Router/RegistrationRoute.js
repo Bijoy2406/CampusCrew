cd094585885b5b78009b19e36ae55c5ceb4bde41
Asif A Khuda <asif13.aak@gmail.com>
2025-08-23 16:15:33 +0600

    Added: Registration Route completed

--- backend/Router/RegistrationRoute.js
+++ backend/Router/RegistrationRoute.js
@@ -25,8 +25,52 @@ router.get('/registrations', async (req, res) => {
 })
 
 
+router.get('/registrations/user/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const registration = await Registration.find({ userId: id }).populate('eventId')
+        if (!registration || registration.length === 0) {
+            return res.status(404).json({ success: false, message: 'Registered events not found', userId: id });
+        }
+        res.status(200).json({ success: true, registration })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+})
+
+router.get('/registrations/event/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const registration = await Registration.find({ eventId: id }).populate('userId')
+        if (!registration || registration.length === 0) {
+            return res.status(404).json({ success: false, message: 'Registered users not found', eventId: id });
+        }
+        res.status(200).json({ success: true, registration })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+})
 
 
+router.delete('/registrations/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userId = req.body.userId;
+
+        const deletion = await Registration.findById(id)
+        if (deletion.userId.toString() !== userId) {
+            return res.status(404).json({ success: false, message: "User is not authorized." })
+        }
+        const deletionResult = await Registration.findByIdAndDelete(id)
+        if (!deletionResult) {
+            return res.status(404).json({ success: false, message: "Registration not found" })
+        }
+        res.status(200).json({ success: true, message: "Registration deleted Successfully" })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+})
 
 
 
